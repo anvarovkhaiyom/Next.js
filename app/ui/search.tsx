7edf83b1c07b8cbc42edc40bb8fccf41e993e07a
anvarovkhaiyom <anvarov.khaiyom@mail.ru>
2024-03-05 16:11:14 +0300

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,7 +7,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+  function HandleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
     const handleSearch = useDebouncedCallback((term) => {
       console.log(`Searching... ${term}`);
@@ -28,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value);
+          HandleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
